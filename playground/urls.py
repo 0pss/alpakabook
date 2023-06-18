6a715cc237741a42b04d2dda9e970ea3e6aa7369
0pss <patrick.stecher@tum.de>
2023-06-18 17:12:00 +0200

    First user retrieved from Database

--- playground/urls.py
+++ playground/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('', views.userpage),
+    path("<int:user_id>/", views.userpage, name="userpage"),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
+from .models import User_dev
 # Create your views here.
 # request --> response
 
-def say_hello(request):
-    return render(request, "hello.html",{'user': 'Alpacio Haferstroh'})
+def userpage(request, user_id):
+    print(user_id)
+    try:
+        user = User_dev.objects.get(pk=user_id)
+    except User_dev.DoesNotExist:
+        raise Http404("User does not exist")
+    print(user)
+    return render(request, "hello.html",{'user':user})
