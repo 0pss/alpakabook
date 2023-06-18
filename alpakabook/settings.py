fffe2a8d65c881960ed168c1573af43b959fdebf
0pss <patrick.stecher@tum.de>
2023-06-18 16:46:01 +0200

    Database is there

--- alpakabook/settings.py
+++ alpakabook/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,10 +32,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+   #"playground.apps.PlaygroundConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.messages",
+    'django.contrib.sessions',
     "django.contrib.staticfiles",
     "playground",
     'bootstrap5',
@@ -118,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User_dev(models.Model):
+
+    def __str__(self):
+            return self.Name
+
+    Name = models.CharField(max_length=200)
+    Email = models.CharField(max_length=200)
+    FavIce = models.CharField(max_length=200)
+    Shoesize = models.CharField(max_length=200)
+    Age = models.CharField(max_length=200)
+    Hobbies = models.CharField(max_length=200)
+    created_date = models.DateTimeField("date published")
+
